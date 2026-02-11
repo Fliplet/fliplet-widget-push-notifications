e1c3c8c8b41bd9afda347a822b5bbd0131eaa7e7
ysolodka <164353368+ysolodka@users.noreply.github.com>
2026-02-11 15:10:54 +0400

    Revert "Fix/PR-24: Added filter to notification list (#216)"
    This reverts commit adf29ff17107d59437fbc06ef4a3d68b22498911.

--- src/components/NotificationList.vue
+++ src/components/NotificationList.vue
@@ -1,41 +1,11 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-
-        <div class="col-xs-6">
-          <p><a href="#" @click.prevent="createNotification" class="btn btn-primary"><i class="fa fa-fw fa-lg fa-plus"></i> Create new</a></p>
-        </div>
-        <div class="col-xs-6">
-          <p><a href="#" class="btn btn-default show-settings pull-right"><i class="fa fa-fw fa-lg fa-cog"></i> Push notification settings</a></p>
-        </div>
+      <div class="col-xs-6">
+        <p><a href="#" @click.prevent="createNotification" class="btn btn-primary"><i class="fa fa-fw fa-lg fa-plus"></i> Create new</a></p>
       </div>
-    <div class="row" v-if="isNotificationsEmpty">
-
-      <div class="col-xs-9">
-        <div>
-          <ul class="status-pills">
-            <li :class="['pill', { active: currentStatus === 'all' }]">
-              <a href="#" @click.prevent="setStatusFilter('all')">All</a>
-            </li>
-            <li :class="['pill', { active: currentStatus === 'draft' }]">
-              <a href="#" @click.prevent="setStatusFilter('draft')">Draft</a>
-            </li>
-            <li :class="['pill', { active: currentStatus === 'published' }]">
-              <a href="#" @click.prevent="setStatusFilter('published')">Published</a>
-            </li>
-            <li :class="['pill', { active: currentStatus === 'scheduled' }]">
-              <a href="#" @click.prevent="setStatusFilter('scheduled')">Scheduled</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-xs-3">
-        <div class="checkbox checkbox-icon pull-right">
-          <input id="show-timezone" type="checkbox" v-model="showTimezone">
-          <label for="show-timezone">
-            <span class="check"><i class="fa fa-check"></i></span> Show timezones
-          </label>
-        </div>
+      <div class="col-xs-6">
+        <p><a href="#" class="btn btn-default show-settings pull-right"><i class="fa fa-fw fa-lg fa-cog"></i> Push notification settings</a></p>
       </div>
     </div>
     <div class="row">
@@ -52,6 +22,26 @@
             </div>
           </template>
           <template v-else>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="notification-filters">
+                  <ul class="nav nav-pills">
+                    <li :class="{ active: currentStatus === 'all' }"><a href="#" @click.prevent="setStatusFilter('all')">All</a></li>
+                    <li :class="{ active: currentStatus === 'draft' }"><a href="#" @click.prevent="setStatusFilter('draft')">Draft</a></li>
+                    <li :class="{ active: currentStatus === 'published' }"><a href="#" @click.prevent="setStatusFilter('published')">Published</a></li>
+                    <li :class="{ active: currentStatus === 'scheduled' }"><a href="#" @click.prevent="setStatusFilter('scheduled')">Scheduled</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="checkbox checkbox-icon pull-right">
+                  <input id="show-timezone" type="checkbox" v-model="showTimezone">
+                  <label for="show-timezone">
+                    <span class="check"><i class="fa fa-check"></i></span> Show timezones
+                  </label>
+                </div>
+              </div>
+            </div>
 
             <table class="table table-condensed notification-list">
               <thead>
@@ -173,7 +163,6 @@ export default {
     return {
       isLoading: false,
       notifications: [],
-      isNotificationsEmpty: false,
       instance: null,
       pageCount: 0,
       pageNumber: getPageNumber(),
@@ -244,7 +233,6 @@ export default {
       if (this.currentStatus === status) {
         return;
       }
-
       this.currentStatus = status;
       this.pageNumber = 1;
       this.loadNotifications();
@@ -436,7 +424,6 @@ export default {
       setNotification();
       bus.$emit('set-view', 'form');
     },
-
     loadNotifications(notificationId) {
       if (typeof notificationId === 'number'
         && _.findIndex(this.notifications, { id: notificationId }) === -1
@@ -457,7 +444,9 @@ export default {
       };
 
       if (this.currentStatus !== 'all') {
-        options.status = this.currentStatus;
+        options.status = [this.currentStatus];
+      } else {
+        options.status = ['draft', 'published', 'scheduled'];
       }
 
       return this.instance.poll(options).then((response) => {
@@ -466,10 +455,6 @@ export default {
           this.pageNumber = response.pageCount || 1;
 
           return;
-        }
-
-        if (response.entries.length > 0 && this.currentStatus === 'all') {
-          this.isNotificationsEmpty = true;
         }
 
         this.isLoading = false;
